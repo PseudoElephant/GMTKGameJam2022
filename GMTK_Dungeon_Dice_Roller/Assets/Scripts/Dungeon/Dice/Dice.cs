cb02d7490a8e725377492a2d1401ef5ce0aee5d7
Alessandro Pisa <aleppaulon@gmail.com>
2022-07-17 07:43:43 -0500

    Dice is now functional
    Missing events!

--- GMTK_Dungeon_Dice_Roller/Assets/Scripts/Dungeon/Dice/Dice.cs
+++ GMTK_Dungeon_Dice_Roller/Assets/Scripts/Dungeon/Dice/Dice.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -27,16 +28,54 @@ public class Dice
         IncreaseEnemyDamage,
     }
 
-    public static void SetGoodDice() {
+    public static GoodDice SetGoodDice() {
         int itemCount = System.Enum.GetValues(typeof(Dice.GoodDice)).Length;
         System.Random rnd = new System.Random();
-        LevelManager.AddGoodDice((Dice.GoodDice)rnd.Next(itemCount));
+        
+        Dice.GoodDice currentDice = (Dice.GoodDice)rnd.Next(itemCount);
+        LevelManager.AddGoodDice(currentDice);
+        return currentDice;
     }
 
-    public static void SetBadDice() {
+    public static BadDice SetBadDice() {
         int itemCount = System.Enum.GetValues(typeof(Dice.BadDice)).Length;
         System.Random rnd = new System.Random();
-        LevelManager.AddBadDice((Dice.BadDice)rnd.Next(itemCount));
+        
+        Dice.BadDice currentDice = (Dice.BadDice)rnd.Next(itemCount);
+        LevelManager.AddBadDice(currentDice);
+        return currentDice;
+    }
+
+    public static string GetGoodDiceString(GoodDice dice)
+    {
+        return dice switch
+        {
+            GoodDice.ExtraLife => "Extra Life!",
+            GoodDice.IncreaseSpeed => "Speed Increased",
+            GoodDice.IncreaseDamage => "Damage Increase",
+            GoodDice.LaserBeam => "Laser Beam!",
+            GoodDice.ExtraShot => "Extra Shot!",
+            GoodDice.IncreaseAttackSpeed => "Increased Attack Speed",
+            GoodDice.FasterBulletSpeed => "Faster Bullet Speed",
+            GoodDice.NukeGame => "Nuke Em'!",
+            GoodDice.LowerDashCooldown => "Faster Dash Cooldown",
+            _ => throw new ArgumentOutOfRangeException(nameof(dice), dice, null)
+        };
+    }
+
+    public static string GetBadDiceString(BadDice dice)
+    {
+        return dice switch
+        {
+            BadDice.FasterEnemies => "Faster Enemies",
+            BadDice.SpawnEnemy => "Spawn Random Enemy",
+            BadDice.NoDash => "No More Dash",
+            BadDice.SpawnBomb => "Spawn Bomb!",
+            BadDice.DuplicateEnemies => "Doppelganger Time",
+            BadDice.IncreaseEnemyHealth => "Increased Enemy Health",
+            BadDice.IncreaseEnemyDamage => "Increased Enemy Damage",
+            _ => throw new ArgumentOutOfRangeException(nameof(dice), dice, null)
+        };
     }
 
 }
---
+++ GMTK_Dungeon_Dice_Roller/Assets/Scripts/GUI/DiceDisplay.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = System.Random;
+
+public class DiceDisplay : MonoBehaviour
+{
+    public GameObject diceObjectPlaceHolder;
+    private Image _image;
+    private Vector2 _originalScale;
+
+    public TextMeshProUGUI text;
+
+    public Sprite[] goodDice;
+
+    public Sprite[] badDice;
+
+    private Sprite[] spritesDice;
+    private bool _isEvilDice;
+        
+    private void Awake()
+    {
+        _originalScale = diceObjectPlaceHolder.transform.localScale;
+        _image = diceObjectPlaceHolder.GetComponent<Image>();
+        LeanTween.scaleX(diceObjectPlaceHolder, 0f, 0f).setEaseInOutSine();
+        LeanTween.scaleY(text.gameObject, 0f, 0f);
+        
+        RegisterEvents();
+    }
+
+    private void PauseAndShowCanvas(bool isEvil)
+    {
+        _isEvilDice = isEvil;
+        spritesDice = (isEvil) ? badDice : goodDice;
+        _image.sprite = spritesDice[0];
+        
+        LevelManager.BroadcastEvent(LevelManager.Event.DiceRoll);
+        
+        LeanTween.scaleX(diceObjectPlaceHolder, _originalScale.x, 0.7f).setEaseInOutSine().setOnComplete(FlipDice);
+    }
+
+    private void FlipDice()
+    {
+        int _finalSprite = 0;
+        if (_isEvilDice)
+        {
+            Dice.BadDice dice = Dice.SetBadDice();
+            _finalSprite = (int)dice % 6;
+            text.text = Dice.GetBadDiceString(dice);
+            
+            text.outlineWidth = 0.1f;
+            text.outlineColor = new Color32(255, 0, 68, 255);
+        }
+        else
+        {
+            Dice.GoodDice dice = Dice.SetGoodDice();
+            _finalSprite = (int)dice % 6;
+            text.text = Dice.GetGoodDiceString(dice);
+            
+            text.outlineWidth = 0.1f;
+            text.outlineColor = new Color32(0, 149, 233, 255);
+        }
+        
+        LeanTween.scaleX(diceObjectPlaceHolder, 0f, 0.3f).setEaseInOutSine().setOnComplete(() =>
+        {
+            _image.sprite = spritesDice[(_finalSprite + 2) % 6];
+            LeanTween.scaleX(diceObjectPlaceHolder, _originalScale.x, 0.3f).setEaseInOutSine().setOnComplete(() =>
+            {
+                LeanTween.scaleX(diceObjectPlaceHolder, 0f, 0.3f).setEaseInOutSine().setOnComplete(() =>
+                {
+                    _image.sprite = spritesDice[(_finalSprite + 4) % 6];
+                    LeanTween.scaleX(diceObjectPlaceHolder, _originalScale.x, 0.3f).setEaseInOutSine().setOnComplete(() =>
+                    {
+                        LeanTween.scaleX(diceObjectPlaceHolder, 0f, 0.3f).setEaseInOutSine().setOnComplete(() =>
+                        {
+                            _image.sprite = spritesDice[(_finalSprite + 5) % 6];
+                            LeanTween.scaleX(diceObjectPlaceHolder, _originalScale.x, 0.3f).setEaseInOutSine().setOnComplete(() =>
+                            {
+                                LeanTween.scaleX(diceObjectPlaceHolder, 0f, 0.3f).setEaseInOutSine().setOnComplete(() =>
+                                {
+                                    _image.sprite = spritesDice[(_finalSprite) % 6];
+                                    LeanTween.scaleY(text.gameObject, 1f, 0.3f).setEaseOutSine();
+                                    LeanTween.scaleX(diceObjectPlaceHolder, _originalScale.x, 0.3f).setEaseInOutSine().setOnComplete(
+                                        () =>
+                                        {
+                                            LeanTween.scale(diceObjectPlaceHolder,
+                                                new Vector3(_originalScale.x, _originalScale.y, 1f) + (Vector3.one * 0.1f), 0.5f).setEaseInOutSine().setOnComplete(() =>
+                                            {
+                                                LeanTween.scale(diceObjectPlaceHolder, _originalScale, 0.3f)
+                                                    .setEaseInOutSine().setOnComplete(HideDice);
+                                            });
+                                        });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    }
+
+    private void HideDice()
+    {
+        LeanTween.scaleY(text.gameObject, 0f, 0.3f).setEaseOutSine();
+        LeanTween.scaleX(diceObjectPlaceHolder, 0f, 0.3f).setEaseInOutSine();
+    }
+    
+    public void TriggerGoodDice()
+    {
+        PauseAndShowCanvas(false);
+    }
+
+    public void TriggerBadDice()
+    {
+        PauseAndShowCanvas(true);
+    }
+
+    private void RegisterEvents()
+    {
+        LevelManager.OnGoodDiceRoll += TriggerGoodDice;
+        LevelManager.OnBadDiceRoll += TriggerBadDice;
+    }
+}
